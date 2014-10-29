24046f86a059f2ebcbd5f503b7413c3ff496e870
dmytro <tangerbomber@gmail.com>
2014-10-29 12:51:03 +0200

    Lesson 1. Step 1

--- MathematicalCalculator/src/main/test/com/teamdev/arseniuk/CalculationTest.java
+++ MathematicalCalculator/src/main/test/com/teamdev/arseniuk/CalculationTest.java
@@ -1,101 +1,98 @@
 package com.teamdev.arseniuk;
 
 import com.teamdev.arseniuk.impl.Calculator;
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.Test;
 
-public class CalculationTest extends TestCase {
+import static org.junit.Assert.assertEquals;
 
-    public CalculationTest(String testName) {
-        super(testName);
-    }
+public class CalculationTest {
 
-    public static Test suite() {
-        return new TestSuite(CalculationTest.class);
-    }
-
+    @Test
     public void testNumber() throws Exception {
         final double expected = 200;
         final String input = "200";
         Calculator calculator = new Calculator();
-        assertEquals(expected, calculator.calculate(input));
+        assertEquals(expected, calculator.calculate(input), 0);
     }
 
+    @Test
     public void testDotFirstNumber() throws Exception {
         final double expected = .200;
         final String input = ".200";
         Calculator calculator = new Calculator();
-        assertEquals(expected, calculator.calculate(input));
+        assertEquals(expected, calculator.calculate(input), 0);
     }
 
-
+    @Test
     public void testSimpleAddition() throws Exception {
         final double expected = 200;
         final String input = "100 + 100";
         Calculator calculator = new Calculator();
-        assertEquals(expected, calculator.calculate(input));
+        assertEquals(expected, calculator.calculate(input), 0);
     }
 
+    @Test
     public void testSimpleSubtraction() throws Exception {
         final double expected = 50;
         final String input = "150 - 100";
         Calculator calculator = new Calculator();
-        assertEquals(expected, calculator.calculate(input));
+        assertEquals(expected, calculator.calculate(input), 0);
     }
 
-
+    @Test
     public void testSimpleDivision() throws Exception {
         final double expected = 30;
         final String input = "90 / 3";
         Calculator calculator = new Calculator();
-        assertEquals(expected, calculator.calculate(input));
+        assertEquals(expected, calculator.calculate(input), 0);
     }
 
-
+    @Test
     public void testSimpleMultiplication() throws Exception {
         final double expected = 100;
         final String input = "20 * 5";
         Calculator calculator = new Calculator();
-        assertEquals(expected, calculator.calculate(input));
+        assertEquals(expected, calculator.calculate(input), 0);
     }
 
-
+    @Test
     public void testSimpleInvolution() throws Exception {
         final double expected = 1024;
         final String input = "2 ^ 10";
         Calculator calculator = new Calculator();
-        assertEquals(expected, calculator.calculate(input));
+        assertEquals(expected, calculator.calculate(input), 0);
     }
 
-
+    @Test
     public void testCalculationWithDifferentPriority() throws Exception {
         final double expected = 70;
         final String input = "10 + 20 * 3";
         Calculator calculator = new Calculator();
-        assertEquals(expected, calculator.calculate(input));
+        assertEquals(expected, calculator.calculate(input), 0);
     }
 
+    @Test
     public void testCalculationWithParenthesis() throws Exception {
         final double expected = 90;
         final String input = "(10 + 20 )* 3";
         Calculator calculator = new Calculator();
-        assertEquals(expected, calculator.calculate(input));
+        assertEquals(expected, calculator.calculate(input), 0);
     }
 
+    @Test
     public void testCalculationWithInnerParenthesis() throws Exception {
         final double expected = -21;
         final String input = "(10 + (1*3) - 20 )* 3";
         Calculator calculator = new Calculator();
-        assertEquals(expected, calculator.calculate(input));
+        assertEquals(expected, calculator.calculate(input), 0);
     }
 
-
+    @Test(expected = CalculationException.class)
     public void testIncorrectParenthesis() throws Exception {
-        final double expected = -21;
-        final String input = "(10 + (1*3) - 20 )* 3";
+        final String input = "(10 + (1*3) - 20 * 3";
         Calculator calculator = new Calculator();
-        assertEquals(expected, calculator.calculate(input));
+        calculator.calculate(input);
     }
+
 
 }
--- src/main/java/com/teamdev/arseniuk/ConsoleCalculator.java
+++ src/main/java/com/teamdev/arseniuk/ConsoleCalculator.java
@@ -7,12 +7,18 @@ import java.util.Scanner;
 
 public class ConsoleCalculator {
 
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         Calculator calculator = new Calculator();
         Scanner scanner = new Scanner(System.in);
         String expression = scanner.nextLine();
-        double result = calculator.calculate(expression);
-        System.out.println(result);
+        double result;
+        try {
+            result = calculator.calculate(expression);
+            System.out.println(result);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
+
 
     }
 }
